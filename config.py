f34d076cbc3e1510875b17c63ade65d21f8ead3e
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2023-07-31 18:52:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("14631157", ))
+    API_ID = int(os.getenv('14631157', ))
     API_HASH = os.getenv("aa7c2b3be68a7488abdb9de6ce78d311", '')
     BOT_TOKEN = os.getenv("5927429053:AAGNa4VeQEOM5_Fj_mmq-21OqB6qr308DDU", '')
